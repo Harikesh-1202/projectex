e5f0c27f5ed4287158506e0c257fd1fa1963f8db
Harikesh-1202 <harikeshreddy7@gmail.com>
2022-12-29 16:51:22 +0530

    my commit ms

---
+++ src/main/java/projectex/mavenfff/App.java
@@ -0,0 +1,13 @@
+package projectex.mavenfff;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
